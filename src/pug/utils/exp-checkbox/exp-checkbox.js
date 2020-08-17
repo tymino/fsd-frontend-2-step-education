17141a5fe33cdbbda55115c97ee503830c5ea077
tymino <d-team-lex@bk.ru>
2020-08-17 14:56:02 +0300

    exp-checkbox done

---
+++ src/pug/utils/exp-checkbox/exp-checkbox.js
@@ -0,0 +1,5 @@
+const arrowExpCheckbox = document.querySelector('.exp-checkbox__arrow-down');
+
+arrowExpCheckbox.addEventListener('click', (e) => {
+  document.querySelector('.exp-checkbox__checkbox-list-container').classList.toggle("exp-checkbox__checkbox-list-container--active");
+});
