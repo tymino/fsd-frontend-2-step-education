1eb53a2e046b296d26b3fba277362037acaff5f2
tymino <d-team-lex@bk.ru>
2020-08-22 17:45:57 +0300

    range slider done

--- src/pug/utils/range-slider/range-slider.js
+++ src/pug/utils/range-slider/range-slider.js
@@ -1,11 +1,16 @@
 const lowerSlider = document.querySelector('#lower');
 const upperSlider = document.querySelector('#upper');
+const rangeColor = document.querySelector('.range-slider__span-color');
 const divCostSlider = document.querySelector('.range-slider__cost');
 
-let lowerVal = parseInt(lowerSlider.value);
-let upperVal = parseInt(upperSlider.value);
-
 const STEP = parseInt(lowerSlider.step);
+const MAXVAL = STEP / 1000;
+
+const setRangeColor = () => {
+  rangeColor.style.marginLeft = parseInt(lowerSlider.value * MAXVAL) + '%';
+  rangeColor.style.width = parseInt(upperSlider.value * MAXVAL) - parseInt(lowerSlider.value * MAXVAL) + '%';
+};
+setRangeColor();
 
 const convertCost = (value) => {
   return value.toLocaleString('ru-RU', {
@@ -23,29 +28,31 @@ const setCost = () => {
 }
 
 upperSlider.addEventListener('input', () => {
-  let lowerVal = parseInt(lowerSlider.value);
-  let upperVal = parseInt(upperSlider.value);
+  lowerVal = parseInt(lowerSlider.value);
+  upperVal = parseInt(upperSlider.value);
 
   if (upperVal < lowerVal + STEP) {
     lowerSlider.value = upperVal - STEP;
-    
+
     if (lowerSlider.value == lowerSlider.min) {
-      upperSlider.value = parseInt(upperSlider.value) + STEP;
+      upperSlider.value = STEP;
     }
   }
   setCost();
+  setRangeColor();
 });
 
 lowerSlider.addEventListener('input', () => {
-  let lowerVal = parseInt(lowerSlider.value);
-  let upperVal = parseInt(upperSlider.value);
+  lowerVal = parseInt(lowerSlider.value);
+  upperVal = parseInt(upperSlider.value);
 
   if (lowerVal > upperVal - STEP) {
     upperSlider.value = lowerVal + STEP;
-      
+    
     if (upperSlider.value == upperSlider.max) {
       lowerSlider.value = parseInt(upperSlider.max) - STEP;
     }
   }
   setCost();
+  setRangeColor();
 });
