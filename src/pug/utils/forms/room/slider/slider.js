fba7b0b527b36e2c64f2276283501cba88b86f94
tymino <d-team-lex@bk.ru>
2020-09-05 16:16:09 +0300

    search room - right column done | need fix all

--- src/pug/utils/forms/room/slider/slider.js
+++ src/pug/utils/forms/room/slider/slider.js
@@ -1,34 +1,34 @@
-// let slideIndex = 1;
-// showSlides(slideIndex);
+let slideIndex = 1;
+showSlides(slideIndex);
 
-// document.addEventListener('click', (e) => {
-//   if (e.target.classList.contains('slider__prev')) {
-//     showSlides(slideIndex -= 1);
-//   } else if (e.target.classList.contains('slider__next')) {
-//     showSlides(slideIndex += 1);
-//   } else if (e.target.classList.contains('slider__dots-item')) {
-//     showSlides(slideIndex = parseInt(e.target.dataset.index));
-//   } else {
-//     return;
-//   }
-// });
+document.addEventListener('click', (e) => {
+  if (e.target.classList.contains('slider__prev')) {
+    showSlides(slideIndex -= 1);
+  } else if (e.target.classList.contains('slider__next')) {
+    showSlides(slideIndex += 1);
+  } else if (e.target.classList.contains('slider__dots-item')) {
+    showSlides(slideIndex = parseInt(e.target.dataset.index));
+  } else {
+    return;
+  }
+});
 
-// function showSlides(n) {
-//     const slides = document.getElementsByClassName("slider__item");
-//     const dots = document.getElementsByClassName("slider__dots-item");
+function showSlides(n) {
+    const slides = document.getElementsByClassName("slider__item");
+    const dots = document.getElementsByClassName("slider__dots-item");
 
-//     if (n > slides.length) {
-//       slideIndex = 1
-//     }
-//     if (n < 1) {
-//         slideIndex = slides.length
-//     }
-//     for (let i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
-//     }
-//     for (let i = 0; i < dots.length; i++) {
-//         dots[i].className = dots[i].className.replace(" slider__dots-item--active", "");
-//     }
-//     slides[slideIndex - 1].style.display = "block";
-//     dots[slideIndex - 1].className += " slider__dots-item--active";
-// }
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" slider__dots-item--active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " slider__dots-item--active";
+}
